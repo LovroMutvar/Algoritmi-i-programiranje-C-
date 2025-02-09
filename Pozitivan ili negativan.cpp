bded5de2ed8a8538fc1a3ebd85687239b6b6852b
LovroMutvar <mutvarlovro13@gmail.com>
2025-02-09 22:18:16 +0100

    Add files via upload

---
+++ Pozitivan ili negativan.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int a;
+ cout<<("Uznewsibroj: ");
+
+ cin>>a;
+ if(a<0){
+cout<<("brojje negativan ");
+
+ }
+else if(a>0){
+    cout<<("broj je pšozitivam ");
+}
+else if(a==0){
+   cout<<("broj je nulla");
+
+}
+
+
+
+    return 0;
+}
