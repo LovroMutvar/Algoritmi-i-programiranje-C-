ed8f18903bc3ca251abcc7ede6c6788d286b81f9
LovroMutvar <mutvarlovro13@gmail.com>
2025-02-09 22:32:02 +0100

    Add files via upload

---
+++ Broj u slovo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+printf("Molim Vas unesite prvi broj: ");
+cin>>a;
+printf("Hvala, vrijednost tog broja u ASCII tablici je: %c",a);
+    return 0;
+}
