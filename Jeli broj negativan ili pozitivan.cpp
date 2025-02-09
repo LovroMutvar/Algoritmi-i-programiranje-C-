798fa7d30d11f5d62f005ccdfb5a8e017cf206d2
LovroMutvar <mutvarlovro13@gmail.com>
2025-02-09 22:19:47 +0100

    Add files via upload

---
+++ Jeli broj negativan ili pozitivan.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+cin>>a;
+cout<<"Unesi neki broj: ";
+if(a%2==0){
+    cout<<"Broj je paran";
+}
+else if(a%2!=0){
+    cout<<"Broj je neparan";
+}
+
+
+    return 0;
+}
