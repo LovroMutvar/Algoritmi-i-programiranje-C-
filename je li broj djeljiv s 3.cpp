571167614ccf7d83bec3d42fc013a17b2bc5e8f8
LovroMutvar <mutvarlovro13@gmail.com>
2025-02-10 18:23:06 +0100

    Add files via upload

---
+++ je li broj djeljiv s 3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int a;
+ cout<<"Molim Vas unesite neki broj";
+ cin>>a;
+ if(a%3==0){
+    cout<<"Broj je djeliv s brojem 3";
+ }
+  if(a%3!=0){
+    cout<<"Broj nije djeljiv s brojem 3";
+  }
+    return 0;}
+
