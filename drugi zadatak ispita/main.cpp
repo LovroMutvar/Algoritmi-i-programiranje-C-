76b4627d31d06c6b4fedeaff99f96f3f7eea47ba
LovroMutvar <mutvarlovro13@gmail.com>
2025-02-12 10:58:00 +0100

    Add files via upload

---
+++ drugi zadatak ispita/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a,b;
+cout<<"a b";
+cout<<endl;
+cin>>a>>b;
+int zbroj;
+zbroj= a+b;
+printf("Zbroj %i i %i iznosi %i",a,b,zbroj);
+    return 0;
+}
