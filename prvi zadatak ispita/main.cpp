329e5a975cb089a5719b37b59310adf487ca3942
LovroMutvar <mutvarlovro13@gmail.com>
2025-02-12 10:56:58 +0100

    Add files via upload

---
+++ prvi zadatak ispita/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a,b;
+cout<<"Molim Vas unesite neki broj : ";
+cin>>a;
+b=a%10;
+cout<<"Hvala vam, znamenka jedinice je: "<<b;
+
+    return 0;
+}
