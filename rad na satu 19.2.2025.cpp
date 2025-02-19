60fc6b3217c00b2d6b40ee8363e7bd14ad0deb17
LovroMutvar <mutvarlovro13@gmail.com>
2025-02-19 11:17:16 +0100

    Add files via upload

---
+++ rad na satu 19.2.2025.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+/* int a,b; 1 zadatak
+cin>>a>>b;
+for(int i=a;i<=b;i++){
+    cout<<i<<endl;
+}*/
+/*int a,b; 2 zadatak
+cin>>a>>b;
+for(int i=b;i>=a;i--){
+    cout<<i<<endl;
+}*/
+/*int a,b; 3 zadatak
+cin>>a>>b;
+if (a%2==0){
+    a=a;}
+else{
+    a+=1;}
+for(int i=a;i<=b;i+=2){
+    cout<<i<<endl; /*
+}
+
+
+/*int a,b; 4 zadatak
+cin>>a>>b;
+for(int i=a;i<=b;i+=3){
+    cout<<i<<endl;
+}*/
+/*char a,b; 5 zadatak
+cout<<"Unesi slova engleske abecede";
+cin>>a>>b;
+for(char i=a;i<=b;i++){
+    cout<<i<<endl;
+}*/
+char a,b;
+cout<<"Unesi slova engleske abecede";
+cin>>a>>b;
+for(char i=a;b!=i,;i++){
+    if(i>'z')
+    i='a';
+    cout<<i<<endl;
+}
+
+}
+    return 0;
+}
